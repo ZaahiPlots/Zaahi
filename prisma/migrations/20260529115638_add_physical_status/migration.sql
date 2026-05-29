35f7b36fdc3b8a73a20a16398d6cac29b693c6fd
Zhan Ryspayev <zhan@zaahi.io>
2026-05-29 16:47:36 +0400

    Revert "feat: physicalStatus migration"
    This reverts commit 876631334a5272064fba9de1c3fba3183aa10aa3.

--- prisma/migrations/20260529115638_add_physical_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "PhysicalStatus" AS ENUM ('EMPTY', 'UNDER_CONSTRUCTION', 'COMPLETED', 'SUSPENDED', 'FUTURE_DEVELOPMENT', 'PRE_CONSTRUCTION');
-
--- AlterTable
-ALTER TABLE "Parcel" ADD COLUMN     "physicalStatus" "PhysicalStatus";
